ca1e52a71a0c3d439038e32c8ba176af8dde2a33
YahirZ01 <s22120159@alumnos.itsur.edu.mx>
2025-05-26 17:36:22 -0600

    c

--- Fire/modelos/usuario.php
+++ Fire/modelos/usuario.php
@@ -3,11 +3,6 @@
     public $id=0;
     public $nombre="";
     public $apellidos="";
-<<<<<<< HEAD
-=======
-    public $edad="";
-    public $email="";
->>>>>>> b3fa258 (cam)
     public $password="";
     public $edad=0;
     public $gmail="";
