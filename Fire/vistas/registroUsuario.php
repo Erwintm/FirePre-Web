26d199e10c20d57513374a1f17e206721b1f222f
YahirZ01 <s22120159@alumnos.itsur.edu.mx>
2025-05-26 17:24:26 -0600

    regitrousuario

---
+++ Fire/vistas/registroUsuario.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro usuario</title>
+</head>
+
+<body>
+    <form action="">
+        <Legend>Registro usuario</Legend>
+        <div>
+            <label for="txtNombre">Nombre</label>
+            <input type="text" id="txtNombre" minlength="2" maxlength="30" require>
+        </div>
+    </form>
+</body>
+
+</html>
