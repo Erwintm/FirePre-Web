56023989af17660c22e252595b4845d137236114
YahirZ01 <s22120159@alumnos.itsur.edu.mx>
2025-05-26 16:41:40 -0600

    inc

---
+++ Fire/modelos/incendio.php
@@ -0,0 +1,18 @@
+<?php 
+    class Incendio{
+        public $id=0;
+        public $fecha="";
+        public $temperatura="";
+        public $velocidad_viento="";
+        public $elevacion="";
+        public $latitud="";
+        public $longitud="";
+        public $tipo_vegetacion="";
+        public $causas="";
+        public $id_zona="";
+        public $humedad="";
+        public $precipitacion="";
+        public $distancia_agua="";
+
+    }
+?>
